cbbe2a70decd16785c84e0557bc9ad75f55a4f86
jazmandorf <jaz@mzcscui-MacBookPro.local>
2020-10-23 15:54:02 +0900

    add reduec

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,63 +1,87 @@
-import React,{useState} from 'react';
+import React from 'react';
 
 //import './App.css';
 import CreateUser from './components/CreateUser'
 import Counter from './components/Counter'
 import UserList from './components/UserList'
+
+function reducer(state, action){
+  switch(action.type){
+    case 'CREATE':
+      return state.concat(action.user)
+    case 'CHANGE':
+      return {...state.inputs,[action.name]:action.value}
+    default:
+      return state 
+  }
+}
 function App() {
+  // reducer 의 기본 틀을 만들기 위해서는 기존에 사용할  여러 state 값들을 객체 형태로 만들어서 만들어 주는게 좋다.
+  // 따라서 내가 여러개의 state 값을 쓰기로 했다면 초기의 state 값이 될 여러 객체를 만들어 놓는게 좋다.
+  const initialState = {
+    inputs : {
+      username : '',
+      email : ''
+    },
+    users : [{
+      id: 1,
+      username: 'velopert',
+      email: 'public.velopert@gmail.com',
+      active: true
+    },
+    {
+      id: 2,
+      username: 'tester',
+      email: 'tester@example.com',
+      active: false
+    },
+    {
+      id: 3,
+      username: 'liz',
+      email: 'liz@example.com',
+      active: false
+    }
+  ]}
  
-  const [inputs, setInputs] = useState({
-    username:'',
-    email : ''
-  });
+
+
+const[state, dispatch] = React.useReducer(reducer,initialState)
 const cur_id = React.useRef(3)
-  const [users, setUsers] = useState([{
-    id: 1,
-    username: 'velopert',
-    email: 'public.velopert@gmail.com',
-    active: true
-  },
-  {
-    id: 2,
-    username: 'tester',
-    email: 'tester@example.com',
-    active: false
-  },
-  {
-    id: 3,
-    username: 'liz',
-    email: 'liz@example.com',
-    active: false
-  }])
+// 요 아래 값들이  setState할때 사용했던 값들 하나로 통합을 위해 initialState에 객체 타입으로 해서 넣어 놓음
+const {users} = state
+//const {username, email} = state.inputs
   
 
   const onChange = (e)=>{
-    const {value, name} = e.target
-    setInputs({
-      ...inputs,
-      [name]:value,
-    })
+    const {name, value} = e.target
+    dispatch({
+      type:"CHANGE",
+      name,
+      value
+
+    }) 
   }
 
-  const onCreate = ()=>{
-    
-    const new_user = {
-      id : cur_id.current+1,
-      username : inputs.username,
-      email: inputs.email,
-      active:false,
-    }
-    setUsers(users=>users.concat(new_user))
-    console.log("cur id : ",cur_id)
+  const onCreate = (e)=>{
+   dispatch({
+     type:'CREATE',
+     user : {
+       id:cur_id.current += 1,
+       username : state.inputs.username,
+       email :state.inputs.email,
+       active : true
+
+     }
+   })
   }
 
   const onRemove = (userId)=>{
-    setUsers(users=>users.filter(user=>user.id !== userId))
+    //setUsers(users=>users.filter(user=>user.id !== userId))
   }
 
   const onToggle = (userId)=>{
-    setUsers(prevUser=>prevUser.map(user=> user.id === userId ? {...user, active:!user.active}:user)
-    )
+    //setUsers(prevUser=>prevUser.map(user=> user.id === userId ? {...user, active:!user.active}:user)
+    //)
   }
   return (
    <>
